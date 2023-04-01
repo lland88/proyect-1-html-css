a23fe664730c30485e6558def076b2c80ad45c35
luigi <llandaeta888@gmail.com>
2023-04-01 14:17:25 -0400

    practicando JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const summary = document.getElementsByClassName(`summary`);
+console.log(summary);
+
+const xde100 = document.getElementsByClassName(`xde100`);
+console.log(xde100);
+
+const resultado = document.querySelector(`.resultado`);
+console.log(resultado);
